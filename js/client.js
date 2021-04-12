8864fbb07a75ae1bedb1fea37db95c5358a3457b
GautamSinghh <gautamsingh@vocvision.com>
2021-04-12 21:45:17 +0530

    completed

--- js/client.js
+++ js/client.js
@@ -1,16 +1,44 @@
 const socket = io('http://localhost:8000');
-
+//GET DOM ELEMENTS IN RESPECTIVE JS VARIABLES 
 const form = document.getElementById('send-container');
 const messageInput =document.getElementById('messageInp');
 const messageContainer = document.querySelector('.container');
 
+//AUDIO THAT WILL B PLAY A RECEVING A MESSAGES//
+var audio = new Audio('tone.mp3');
+
+//FUNCTION WHICH WILL APPEND EVENT INFO TO THE CONTAINER
 const append = (message , position) =>{
     const messageElement = document.createElement('div');
     messageElement.innerText = message;
     messageElement.classList.add('message');
     messageElement.classList.add(position);
     messageContainer.append(messageElement);
+    if(position== 'left') {
+        audio.play();
+    }
 }
+
+// ASK A NEW USER FOR HIS NAME AND LET THE SERVER KNOW//
+const name = prompt('enter your name');
+socket.emit('new-user-joined', name);
+
+//IF A NEW USERS JOINS, RECEIVE HER NAME TO SERVER//
+socket.on('user-joined', name=>{
+    append(`${name} joined the chat`, 'right');
+})
+
+// IF SERVER SEND A MESSAGE ,RECEIVE IT//
+socket.on('receive', data=>{
+    append(`${data.name}: ${data.message}`, 'left');
+})
+
+//IF USERS LEAVES THE CHAT, APPENT THE INFO TO CONTAINER//
+socket.on('left', name=>{
+    append(`${name} left the chat`, 'left');
+})
+
+//IF THE FORM SUBMITTED, SEND SERVER TO TH MESSAGE//
 form.addEventListener('submit', (e) =>{
     e.preventDefault();
     const message = messageInput.value;
@@ -18,16 +46,3 @@ form.addEventListener('submit', (e) =>{
     socket.emit('send', message);
     messageInput.value = '';
 })
-const name = prompt('enter your name');
-socket.emit('new-user-joined', name);
-
-socket.on('user-joined', name=>{
-    append(`${name} joined the chat`, 'right');
-})
-socket.on('receive', data=>{
-    append(`${data.name}: ${data.message}`, 'left');
-})
-socket.on('left', name=>{
-    append(`${name} left the chat`, 'left');
-})
-
--- nodeserver/index.js
+++ nodeserver/index.js
@@ -1,7 +1,3 @@
-// const { Server } = require('socket.io');
-
-//node server which wil handal socket io connection//
-// const io = require("socket.io")(8000);
 const io = require("socket.io")(8000, {
   cors: {
     origin: "http://localhost:5500",
@@ -12,15 +8,17 @@ const io = require("socket.io")(8000, {
 const users = {};
 
 io.on('connection', socket => {
+  //IF ANY NEW USER JOINS , LET OTHER USERS CONNECTED TO THE SERVER KNON//
   socket.on('new-user-joined', name => {
-    console.log("new-user", name);
+    // console.log("new-user", name);   
     users[socket.id] = name;
     socket.broadcast.emit('user-joined', name);
   });
+  //IS SOMEONE ONE A MESSAGE, BRODCAST IT TO OTHER PEOPLE//
   socket.on('send', message => {
     socket.broadcast.emit('receive', { message: message, name: users[socket.id] })
   })
-
+ //IF SOME ONE LEVES THE CHAT, LET OTHER KNOWW//
   socket.on('disconnect', message => {
     socket.broadcast.emit('left', users[socket.id])
     delete users[socket.id]
